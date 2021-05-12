1ded81661070dd4b029958a3585c0634a576a044
[lee sampson] <[emailme@leesampson.co.nz]>
2021-05-13 07:05:05 +1200

    up to https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_interactivity_events_state

--- src/AddButton.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function AddButton() {
-  return <button>Add</button>;
-}
-
-export default AddButton;
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Hello from './Hello';
-import Card from './Card';
+// import Card from './Card';
 import Images from './Images';
 import List from './List';
 import Today from './Today';
 import Footer from './Footer';
+import Todo from './Todo';
+import FilterButton from './FilterButton';
+import Form from './Form';
+import { nanoid } from 'nanoid';
 
-function App() {
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+    />
+  ));
+
+  function addTask(name) {
+    const newTask = { id: 'todo-' + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div>
+      return (
+      <div className="todoapp stack-large">
+        <Form addTask={addTask} />
+        <div className="filters btn-group stack-exception">
+          <FilterButton />
+          <FilterButton />
+          <FilterButton />
+        </div>
+        <h2 id="list-heading">{headingText}</h2>
+        <ul
+          role="list"
+          className="todo-list stack-large stack-exception"
+          aria-labelledby="list-heading"
+        >
+          {taskList}
+        </ul>
+      </div>
+      );
       <Header />
       <Images />
       <Hello />
-
       <Today />
       <List />
-      <Card name="Garden" content="test" />
-      <Card name="Pool Area" content="test" />
-      <Card name="Inside" content="test" />
+      {/* <Card /> */}
       <Footer />
     </div>
   );
